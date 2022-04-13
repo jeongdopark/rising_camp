3a822e4d19df286934ea77b63b92e13407b22f6c
jeongdopark <jdpark0816@gmail.com>
2022-04-13 20:48:26 +0900

    clone coding

---
+++ clone_coding/host-page/host.js
@@ -0,0 +1,67 @@
+
+
+const video = document.querySelector('.first-video');
+const playBtn = document.querySelector('.play-btn');
+const videoSectionImg = document.querySelector('.video1-section-image');
+const videoSectionH = document.querySelector('.video1-section-h3');
+const navSection = document.querySelector('.nav-section')
+const icon = document.querySelector('.icon-wrap');
+let playStatus = false; // True는 재생이 종료된 상태 또는 일시정지 상태
+
+navSection.classList.add('nope')
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+    if(scrollTop > 700){
+        navSection.classList.remove('nope')
+        navSection.classList.add('seen')
+
+    }else{
+        navSection.classList.remove('seen')
+        navSection.classList.add('nope')
+
+        
+    }
+})
+
+// 비디오 상태에 따른 플레이 아이콘 변화
+const videoStatus = () => {
+    if(playStatus == true){
+        video.play()
+        playStatus = false
+        playBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`
+        
+    }else{
+        video.pause()
+        playStatus = true
+        playBtn.innerHTML = `<i class="fa-solid fa-play"></i> `
+    }
+}
+
+// 0.5초 주기로 비디오가 끝났는지 확인하는 함수
+setInterval(() => {
+    currentTime = video.currentTime;
+    if(currentTime < 2.3){
+        videoSectionImg.style.backgroundImage = "url('https://a0.muscache.com/im/pictures/cac9415a-acfb-4c92-a6d0-770e948741b1.jpg?im_w=480')";
+        videoSectionH.innerText = "필라델피아의 호스트";
+    }else if(currentTime < 5){
+        videoSectionImg.style.backgroundImage = "url('https://a0.muscache.com/im/pictures/9849ff03-6166-41c5-bbf7-80c908b4820c.jpg?im_w=480')"
+        videoSectionH.innerText = "뭄바이의 호스트";
+    }else if(currentTime < 7.7){
+        videoSectionImg.style.backgroundImage = "url('https://a0.muscache.com/im/pictures/adf2bfef-5ddc-41d6-86b5-a49d274f4f54.jpg?im_w=480')"
+        videoSectionH.innerText = "요하네스버그의 호스트";
+    }else{
+        videoSectionImg.style.backgroundImage = "url('https://a0.muscache.com/im/pictures/b78b2eb7-2d63-4c8d-9246-6b08c457c9cc.jpg?im_w=480')"
+        videoSectionH.innerText = "멕시코시티의 호스트";
+    }
+    if(video.ended){
+        playStatus = true
+        playBtn.innerHTML = `<i class="fa-solid fa-play"></i>`
+    }
+}, 500)
+
+// 플레이 버튼 클릭시 videoStatus 함수 실행
+playBtn.addEventListener('click', () => {    
+    videoStatus()
+})
+
+
